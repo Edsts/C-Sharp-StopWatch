aab3cf830a0e43a810d3fa3f461d9cc08ef13aec
Ednaldo Santos <ends1008@gmail.com>
2021-01-23 07:52:45 -0300

    Finishing StopWatch

---
+++ StopWatch/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar ?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                Environment.Exit(0);
+
+            Start(time * multiplier);
+
+        }
+
+
+        static void Start(int time)
+        {
+
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(3000);
+            Console.Clear();
+
+            Console.WriteLine("Set...");
+            Thread.Sleep(3000);
+            Console.Clear();
+
+            Console.WriteLine("GO!!!");
+            Thread.Sleep(2000);
+            Console.Clear();
+
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("StopWatch Finalizado !");
+            Thread.Sleep(2500);
+        }
+    }
+}
